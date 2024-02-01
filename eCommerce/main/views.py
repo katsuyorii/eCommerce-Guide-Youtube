ebc8e19d1ebe8327d98181e1d6e7f0ed06553366
katsuyorii <katsuyoriiii@gmail.com>
2024-02-01 10:03:10 +0300

    add CBV

--- eCommerce/main/views.py
+++ eCommerce/main/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, 'main/index.html')
+class Index(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home'
+
+        return context
