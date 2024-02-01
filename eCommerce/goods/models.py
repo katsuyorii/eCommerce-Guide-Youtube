e598b3d561798f87229d9f6be6921126833c9ff4
katsuyorii <katsuyoriiii@gmail.com>
2024-02-01 12:53:28 +0300

    add model and register

--- eCommerce/goods/models.py
+++ eCommerce/goods/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Наименование:', max_length=150, unique=True)
+    slug = models.SlugField(verbose_name='Слаг', unique=True, blank=True, null=True, db_index=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
