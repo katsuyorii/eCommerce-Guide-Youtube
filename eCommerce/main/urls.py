de2fb24536927792c0464653232cdfdde810a240
katsuyorii <katsuyoriiii@gmail.com>
2024-02-01 11:34:26 +0300

    add about html and urls

--- eCommerce/main/urls.py
+++ eCommerce/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index, AboutUs
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('about/', AboutUs.as_view(), name='about'),
 ]
--- eCommerce/main/views.py
+++ eCommerce/main/views.py
@@ -9,3 +9,13 @@ class Index(TemplateView):
         context['title'] = 'Home'
 
         return context
+
+
+class AboutUs(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'About us'
+
+        return context
