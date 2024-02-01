faccd1444eced68f221cb64966e31a090172b16d
katsuyorii <katsuyoriiii@gmail.com>
2024-02-01 15:14:57 +0300

    add models and auto slug

--- eCommerce/goods/models.py
+++ eCommerce/goods/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from pytils.translit import slugify
 
 
 class Category(models.Model):
@@ -8,6 +9,34 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+
+    '''
+    Переопределение метода сохранения записи.
+    С помощью библиотеки pytils мы переводим киррилицу в латиницу по полю name.
+    Здесь поле name уникальное, проблем нет, но если поле не будет уникальным, то нужно добавлять к slug что-то в конец, чтобы не было ошибки.
+    '''
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='Наименование:', max_length=150, unique=True)
+    slug = models.SlugField(verbose_name='Слаг', unique=True, blank=True, null=True, db_index=True)
+    desc = models.TextField(verbose_name='Описание:')
+    image = models.ImageField(verbose_name='Изображение:', upload_to='goods_images', null=True, blank=True)
+    price = models.DecimalField(verbose_name='Цена:', default=0.00, decimal_places=2, max_digits=7)
+    discount = models.DecimalField(verbose_name='Скидка:', default=0.00, decimal_places=2, max_digits=7, help_text='В процентах (%)')
+    count = models.PositiveIntegerField(verbose_name='Количество на складе:', default=0)
+    category = models.ForeignKey(verbose_name='Категория:', to=Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
     def __str__(self):
         return self.name
