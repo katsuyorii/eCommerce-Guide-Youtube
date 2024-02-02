7a843cf4eda7e63cb0402770d1e1b12beddd23cd
katsuyorii <katsuyoriiii@gmail.com>
2024-02-02 20:43:22 +0300

    add base login view and form

--- eCommerce/users/views.py
+++ eCommerce/users/views.py
@@ -1,7 +1,16 @@
 from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView
+from .forms import UserLoginForm
 
-class Login(TemplateView):
+class Login(LoginView):
+    form_class = UserLoginForm
     template_name = 'users/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Login'
+
+        return context
 
 class Registration(TemplateView):
     template_name = 'users/registration.html'
