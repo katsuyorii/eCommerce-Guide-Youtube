eb2e239a8e77bea401f9aa28e449c7b540060d26
katsuyorii <katsuyoriiii@gmail.com>
2024-02-04 19:31:33 +0300

    add login required

--- eCommerce/users/urls.py
+++ eCommerce/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import Login, Registration, Profile, logout_user
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('registration/', Registration.as_view(), name='registration'),
-    path('profile/', Profile.as_view(), name='profile'),
-    path('logout/', logout_user, name='logout'),
+    path('profile/', login_required(Profile.as_view()), name='profile'),
+    path('logout/', login_required(logout_user), name='logout'),
 ]
