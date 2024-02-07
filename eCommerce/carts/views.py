6c5bee5000a7fed05f0edd83c126afd92774c029
katsuyorii <katsuyoriiii@gmail.com>
2024-02-07 10:14:43 +0300

    add function add in cart

--- eCommerce/carts/views.py
+++ eCommerce/carts/views.py
@@ -1,7 +1,24 @@
+from django.shortcuts import redirect, render
 from django.views.generic.detail import DetailView
+from django.views.generic.base import View
+from goods.models import Product
+from .models import Cart
 
-class CartAdd(DetailView):
-    pass
+class CartAdd(View):
+    def get(self, request, product_slug, *args, **kwargs):
+        product = Product.objects.get(slug=product_slug)
+        if request.user.is_authenticated:
+            carts = Cart.objects.filter(user=request.user, product=product)
+            if carts.exists():
+                cart = carts.first()
+                if cart:
+                    cart.quantity += 1
+                    cart.save()
+            else:
+                Cart.objects.create(user=request.user, product=product, quantity=1)
+
+        return redirect(request.META['HTTP_REFERER'])
+
 
 class CartChange(DetailView):
     pass
